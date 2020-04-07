0b9c3be9a6ad374e115af7c60254ce93163b3d9f
Patrick <patrick.yao96@gmail.com>
2020-04-07 19:51:39 +1000

    US8

---
+++ app/src/main/java/com/example/infs3605_group_project/activities/InvestmentStrategy.java
@@ -0,0 +1,13 @@
+package com.example.infs3605_group_project.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InvestmentStrategy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_investment_strategy);
+    }
+}
