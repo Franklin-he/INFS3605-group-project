f2337190d29ee1f8b83b4d7af3a893dc4c4d1c9d
FranklinH <franklin110698@hotmail.com>
2020-04-23 08:09:54 +1000

    line charts added when user clicks on ETFs and Bonds

---
+++ app/src/main/java/com/example/infs3605_group_project/activities/BondsLineChart.java
@@ -0,0 +1,84 @@
+package com.example.infs3605_group_project.activities;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.infs3605_group_project.R;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+public class BondsLineChart extends AppCompatActivity{
+
+    private com.github.mikephil.charting.charts.LineChart lChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bonds);
+        lChart = (com.github.mikephil.charting.charts.LineChart) findViewById(R.id.line_chart_1);
+        lChart.setDragEnabled(true);
+        lChart.setScaleEnabled(false);
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+        SharedPreferences sharedPreferences = getSharedPreferences("incomes", MODE_PRIVATE);
+        Long t_savings = sharedPreferences.getLong("total_savings", 0);
+
+        Double savings = (double) t_savings;
+
+
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        String[] years = new String[20];
+
+        List<String> xAxisValues;
+
+        for (int i = 0; i < 20; i++) {
+            years[i] = String.valueOf(year + i);
+            yValues.add(new Entry(i, Float.valueOf(String.valueOf(savings))));
+            savings = savings + savings * 0.055;
+        }
+
+
+        LineDataSet set1 = new LineDataSet(yValues, "Future savings");
+        set1.setFillAlpha(2000);
+        set1.setLineWidth(2f);
+        set1.setValueTextSize(10f);
+
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+
+        lChart.setData(data);
+        lChart.getAxisRight().setEnabled(false);
+        lChart.getDescription().setEnabled(false);
+
+        XAxis xAxis = lChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        lChart.getXAxis().setValueFormatter(new com.github.mikephil.charting.formatter.IndexAxisValueFormatter(years));
+        xAxis.setGranularity(1);
+
+    }
+
+
+}
--- app/src/main/java/com/example/infs3605_group_project/activities/ShowRecommendInvestment.java
+++ app/src/main/java/com/example/infs3605_group_project/activities/ShowRecommendInvestment.java
@@ -1,7 +1,10 @@
 package com.example.infs3605_group_project.activities;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,16 +14,38 @@ import com.example.infs3605_group_project.R;
 
 public class ShowRecommendInvestment extends AppCompatActivity {
 
+    Button bonds;
+    Button etfs;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend_1_1);
 
         TextView textView = (TextView) findViewById(R.id.textView_savings);
+        bonds = findViewById(R.id.bonds);
+        etfs = findViewById(R.id.funds);
+
         SharedPreferences sharedPreferences = getSharedPreferences("incomes", MODE_PRIVATE);
         Long t_savings = sharedPreferences.getLong("total_savings", 0);
         textView.setText("Make the most of your savings: $" + t_savings + "\n");
 
+        bonds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent intent = new Intent(ShowRecommendInvestment.this, BondsLineChart.class);
+                ShowRecommendInvestment.this.startActivity(intent);
+            }
+
+        });
+        etfs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ShowRecommendInvestment.this, ETFsLineChart.class);
+                ShowRecommendInvestment.this.startActivity(intent);
+            }
+
+        });
     }
 }
